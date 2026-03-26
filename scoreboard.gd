efdf4440f02a046199d86e0e171c5ca5ff3489a4
Acherontia42 <144288535+Acherontia42@users.noreply.github.com>
2026-03-25 20:05:15 -0400

    i have updated things. yay.

---
+++ scoreboard.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#basically this is where we pull the top scores (top 10?)
+	#from the database
+	#and then put them into a bunch of rich text labels or just labels
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
