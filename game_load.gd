15cdcd637061c227e82d8afe3ab3a125b9e3a113
Acherontia42 <144288535+Acherontia42@users.noreply.github.com>
2026-03-28 12:47:06 -0400

    AAAAA

--- game_load.gd
+++ game_load.gd
@@ -3,6 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$Label.text = "THIS IS SUPPOSED TO BE A LOADING SCREEN HIT CONTINUE"
 	#Load in anything that we need
 	#This is the page with your load game, new game, etc data
 	pass # Replace with function body.
@@ -11,3 +12,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_tempnext_pressed() -> void:
+	get_tree().change_scene_to_file("res://Login_Screen.tscn")
