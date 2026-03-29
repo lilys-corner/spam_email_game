114f0adbd32abc66b0974d95de28c24569d5d86c
Acherontia42 <144288535+Acherontia42@users.noreply.github.com>
2026-03-29 10:52:36 -0400

    Added options button

--- scoreboard.gd
+++ scoreboard.gd
@@ -6,10 +6,14 @@ func _ready() -> void:
 	#basically this is where we pull the top scores (top 10?)
 	#from the database
 	#and then put them into a bunch of rich text labels or just labels
-	
+	$tempLabel.text = "SCOREBOARD PAGE"
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Main_Menu.tscn")
