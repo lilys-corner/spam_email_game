a79436d291d725405c059645242b441832b19d10
sneesna <snaasnee@gmail.com>
2026-04-14 13:17:25 -0400

    Added Fullscreen/Windowed commented code
    Commented because I don't know where to put it. I think it's activated by a button. For now, I put it in global.gd below the fullscreen variable

--- global.gd
+++ global.gd
@@ -11,6 +11,19 @@ var dyslexiaFont = false
 var fontSize = 0 #OK we will adjust the default values later when we implement this
 
 var fullScreen = false #Not entirely sure how we implement this though
+'''
+^ For Fullscreen:
+Commented bc idk where to put this but this is the code for swapping
+func _process(_delta):
+	if Input.is_action_just_pressed("full_screen"): // idk what full_screen is exactly tho
+		fullscreen = not fullscreen
+		toggle_fullscreen()
+
+func toggle_fullscreen():
+	if fullscreen:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)'''
 
 var masterVolume = 100.0 #Default is 100% I guess
 var sfxVolume = 100.0
