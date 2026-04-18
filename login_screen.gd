3a5e698fdd7ec7e3b0e6b3444f32e0f88aad4309
sneesna <snaasnee@gmail.com>
2026-04-18 11:15:28 -0400

    Removed temp button
    temp button is now the bottom button (Guest)

--- login_screen.gd
+++ login_screen.gd
@@ -2,20 +2,11 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
-	
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_temp_button_pressed() -> void:
-	#remove later. I don't care. I'm just setting up a skeleton
-	#I'm scared of how the logins will work
-	get_tree().change_scene_to_file("res://Main_Menu.tscn")
 
 func _on_log_in_pressed() -> void:
 	#go to log in screen upon clicking login button (top)
@@ -24,3 +15,6 @@ func _on_log_in_pressed() -> void:
 func _on_sign_up_pressed() -> void:
 	#go to sign up screen upon clicking signup button (top)
 	get_tree().change_scene_to_file("res://Sign_Up.tscn")
+
+func _on_guest_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Main_Menu.tscn")
