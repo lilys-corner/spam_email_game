8a163436673d6fa1f96df4ca26f01f76686d6e9a
Acherontia42 <144288535+Acherontia42@users.noreply.github.com>
2026-04-20 15:56:39 -0400

    Set up leaderboard labels

--- scoreboard.gd
+++ scoreboard.gd
@@ -7,7 +7,73 @@ func _ready() -> void:
 	#from the database
 	#and then put them into a bunch of rich text labels or just labels
 	$tempLabel.text = "SCOREBOARD PAGE"
-	pass # Replace with function body.
+	
+	# DATABASE PULL
+	
+	# WHOO labels and the like
+	#Oh god there's so many
+	# QUIZ LEADERBOARD----------------------->>
+	$qBoard/tier1/username.text = ""
+	$qBoard/tier1/score.text = ""
+	#tier 2
+	$qBoard/tier2/username.text = ""
+	$qBoard/tier2/score.text = ""
+	# rank 3
+	$qBoard/tier3/username.text = ""
+	$qBoard/tier3/score.text = ""
+	# rank 4
+	$qBoard/tier4/username.text = ""
+	$qBoard/tier4/score.text = ""
+	# rank 5
+	$qBoard/tier5/username.text = ""
+	$qBoard/tier5/score.text = ""
+	# rank 6
+	$qBoard/tier6/username.text = ""
+	$qBoard/tier6/score.text = ""
+	# rank 7
+	$qBoard/tier7/username.text = ""
+	$qBoard/tier7/score.text = ""
+	# rank 8
+	$qBoard/tier8/username.text = ""
+	$qBoard/tier8/score.text = ""
+	# rank 9
+	$qBoard/tier9/username.text = ""
+	$qBoard/tier9/score.text = ""
+	# rank 10
+	$qBoard/tier10/username.text = ""
+	$qBoard/tier10/score.text = ""
+	
+	
+	# GAME LEADERBOARD----------------------->>
+	$gBoard/tier1/username.text = ""
+	$gBoard/tier1/score.text = ""
+	#tier 2
+	$gBoard/tier2/username.text = ""
+	$gBoard/tier2/score.text = ""
+	# rank 3
+	$gBoard/tier3/username.text = ""
+	$gBoard/tier3/score.text = ""
+	# rank 4
+	$gBoard/tier4/username.text = ""
+	$gBoard/tier4/score.text = ""
+	# rank 5
+	$gBoard/tier5/username.text = ""
+	$gBoard/tier5/score.text = ""
+	# rank 6
+	$gBoard/tier6/username.text = ""
+	$gBoard/tier6/score.text = ""
+	# rank 7
+	$gBoard/tier7/username.text = ""
+	$gBoard/tier7/score.text = ""
+	# rank 8
+	$gBoard/tier8/username.text = ""
+	$gBoard/tier8/score.text = ""
+	# rank 9
+	$gBoard/tier9/username.text = ""
+	$gBoard/tier9/score.text = ""
+	# rank 10
+	$gBoard/tier10/username.text = ""
+	$gBoard/tier10/score.text = ""
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
