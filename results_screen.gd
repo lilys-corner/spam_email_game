c30305d4041a657c455c1e9cb218b1dd9bd37965
sneesna <snaasnee@gmail.com>
2026-04-22 19:23:49 -0400

    Results Screen
    Score and incorrectly answered questions (and their right answers) are now displayed on the results screen. Also there's a temporary button to bring you to results screen with a score of 80% and 4 incorrect questions (not random)

--- results_screen.gd
+++ results_screen.gd
@@ -1,14 +1,21 @@
 extends Control
+var database: SQLite
+
+
+var questionNum = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	print(Global.incorrectquiz.size())
+	database = SQLite.new()
+	database.path = "res://questionsData.db"
+	database.open_db()
 	
-	# i think this is where we calculate score
+	# bam your score is that global variable
+	$ScoreLabel.text = "Your Score: " + str(Global.quizscore) + "%"
 	
-	# here is where the score will be saved to the leaderboard
-	# it'll save regardless if it's top 10
-	# showing only top 10 is a problem for the leaderboard page
+	# display first question
+	updateQuestion()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,3 +26,53 @@ func _process(delta: float) -> void:
 func _on_back_button_pressed() -> void:
 	# if you're done here go back to the main menu
 	get_tree().change_scene_to_file("res://Main_Menu.tscn")
+
+func _on_last_question_pressed() -> void:
+	#check
+	if questionNum == 0:
+		pass
+		#do nothing. you can't go back
+	else:
+		#decrement
+		questionNum = questionNum - 1
+		
+		#update
+		updateQuestion()
+
+func _on_next_question_pressed() -> void:
+	#check
+	if(questionNum >= Global.incorrectquiz.size() - 1):
+		pass
+		# you are not allowedd
+	else:
+		#increment
+		questionNum = questionNum + 1
+		updateQuestion()
+	#update
+
+func updateQuestion() -> void:
+	# our chosen question is whichever one we're on ig
+	var qID = Global.incorrectquiz[questionNum]
+	
+	# get the correct answer number as an int. is it option 1, 2, 3 or 4
+	var my_query = "SELECT qCorrect FROM questions WHERE qID = " + str(qID) + ";"
+	database.query(my_query)
+	var this_correct = database.query_result[0]["qCorrect"]
+	var this_ans = "qChoice" + str(this_correct)
+	
+	# cool now let's get both the question body and the correct answer !!
+	# overwrite my_query we don't need the last one anymore
+	my_query = "SELECT qBody, " + this_ans + " FROM questions WHERE qID = " + str(qID) + ";"
+	database.query(my_query)
+	
+	# yay we have that. now let's put them in variables
+	var quesBody : String = database.query_result[0]["qBody"]
+	var quesAns : String = database.query_result[0][this_ans]
+	
+	#update:
+	$wrongQuestion.text = quesBody
+	$corrAns.text = quesAns
+	
+	#var tempnum = questionNum + 1
+	#$questionCount.text = "Wrong Question " + str(tempnum) + "/20"
+	#^ updating the text box that shows you how far you are
